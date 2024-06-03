de171a5ca17b0a84fa90e45e021a592fd07e018b
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-06-03 10:28:24 +0530

    Added basic `GLFW` code to open a 800x600 window

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,9 +1,54 @@
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+
+void
+ProcessInput(GLFWwindow *window)
+{
+   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+      glfwSetWindowShouldClose(window, true);
+}
+
+void
+Render()
+{
+   // Render commands here
+}
+
 int
 main()
 {
-   printf("Hello World\n");
+   glfwInit();
+   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);   // MacOS only
+
+   GLFWwindow *window =
+       glfwCreateWindow(800, 600, "Learn OpenGL", nullptr, nullptr);
+   if (!window)
+   {
+      std::cerr << "Failed to create window" << std::endl;
+      glfwTerminate();
+
+      return EXIT_FAILURE;
+   }
+
+   glfwMakeContextCurrent(window);
+
+   while (!glfwWindowShouldClose(window))
+   {
+      ProcessInput(window);
+
+      Render();
+
+      glfwSwapBuffers(window);
+      glfwPollEvents();
+   }
+
+   glfwTerminate();
+
+   return EXIT_SUCCESS;
 
    return 0;
 }
