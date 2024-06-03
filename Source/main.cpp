b4d351ad00b944feba4f19696cfdd794f16c7401
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-06-03 10:29:21 +0530

    Minor changes

--- Source/main.cpp
+++ Source/main.cpp
@@ -22,7 +22,9 @@ main()
    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);   // MacOS only
+#endif
 
    GLFWwindow *window =
        glfwCreateWindow(800, 600, "Learn OpenGL", nullptr, nullptr);
