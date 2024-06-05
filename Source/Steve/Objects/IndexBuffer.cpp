1beda0b82ce6d7f91fe4f19d36c5a37cb5a27e63
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-06-05 10:23:52 +0530

    Added wrapper class `IndexBuffer` for managing index buffers easily

---
+++ Source/Steve/Objects/IndexBuffer.cpp
@@ -0,0 +1,57 @@
+#include "IndexBuffer.hpp"
+
+#include <glad/glad.h>
+#include <iostream>
+
+Result<bool>
+IndexBuffer::BindAndPopulate()
+{
+   if (!m_HasInitialized)
+   {
+      glGenBuffers(1, &m_ID);
+      m_HasInitialized = true;
+   }
+
+   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ID);
+
+   uint32_t indices[MAX_INDICES];
+   for (uint32_t i = 0; i < MAX_QUADS; i++)
+   {
+      // Two traingles per quad: 0, 1, 2 and 2, 3, 0
+      // -------------------------------------------
+      // | 0         1 | 0, 1, 2
+      indices[i * 6 + 0] = i * 4 + 0;
+      indices[i * 6 + 1] = i * 4 + 1;
+      indices[i * 6 + 2] = i * 4 + 2;
+      // | 3         2 | 2, 3, 0
+      indices[i * 6 + 3] = i * 4 + 2;
+      indices[i * 6 + 4] = i * 4 + 3;
+      indices[i * 6 + 5] = i * 4 + 0;
+      // -------------------------------------------
+   }
+
+   glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+                sizeof(indices),
+                indices,
+                GL_STATIC_DRAW);
+
+   return true;
+}
+
+Result<bool>
+IndexBuffer::BindAndUploadData(const std::vector<uint32_t> &data)
+{
+   if (!m_HasInitialized)
+   {
+      glGenBuffers(1, &m_ID);
+      m_HasInitialized = true;
+   }
+
+   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ID);
+   glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+                data.size() * sizeof(uint32_t),
+                data.data(),
+                GL_STATIC_DRAW);
+
+   return true;
+}
