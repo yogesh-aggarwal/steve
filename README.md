07e78b326919c1b8fa9f378cb3b89a535e934ec0
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-06-08 12:45:53 +0530

    Renamed everything else to "Steve"

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Learn OpenGL 3.3
-
-This repository contains the source code written by following the tutorials of [learnopengl.com](https://learnopengl.com).
+# Steve Browser
---
+++ Scripts/setup-debian.sh
@@ -0,0 +1,8 @@
+sudo apt update
+
+sudo apt install -y \
+   make cmake pkg-config clang-format build-essential \
+   libssl-dev libarchive-dev libwayland-dev \
+   libxkbcommon-dev libxkbcommon-x11-dev libx11-dev \
+   libxinerama-dev libxcursor-dev libxi-dev libxrandr-dev \
+   libxss-dev libxtst-dev libxkbfile-dev
