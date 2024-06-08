a5d3c9bc65dd2fd197abf2a67e5c736c58c89f60
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-06-08 21:50:21 +0530

    Added files for managing graphics context. For now only created files but in future after an MVP, the implementation will be done for better understanding of the graphics context.

---
+++ Source/Steve/Renderer/Context/OpenGL.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "GraphicsContext.hpp"
+
+class OpenGLContext : public GraphicsContext
+{
+};
---
+++ Source/Steve/Renderer/Context/Vulkan.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "GraphicsContext.hpp"
+
+class VulkanContext : public GraphicsContext
+{
+};
