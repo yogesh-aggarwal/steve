5242c5f9950642347e7d071770111f9ab3e84a1a
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-12-12 12:32:53 +0530

    Add debugging macros and define limits for rendering

--- source/steve/core/common/defines.hpp
+++ source/steve/core/common/defines.hpp
@@ -4,3 +4,11 @@
    if (result.is_error()) {      \
       return result.get_error(); \
    }
+
+#define DLOG std::cout << __FILE__ << ":" << __LINE__ << " " << __func__ << std::endl
+#define DGLERROR \
+   std::cout << __FILE__ << ":" << __LINE__ << " " << __func__ << " " << glGetError() << std::endl
+
+#define MAX_QUADS    1000
+#define MAX_VERTICES (MAX_QUADS * 4)
+#define MAX_INDICES  (MAX_QUADS * 6)
