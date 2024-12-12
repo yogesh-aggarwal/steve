79891aa3c58b94cdb34265f1cd2ee357710c7768
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-12-12 14:10:44 +0530

    Add namespace for drawing functions for generating quad vertices

---
+++ source/steve/lib/graphics/draw.cpp
@@ -0,0 +1,26 @@
+#include "draw.hpp"
+
+using namespace steve;
+
+/* ------------------------------------------------------------------------------------------------------- */
+
+std::array<Vertex, 4>
+Draw::DrawQuad(float x, float y, float width, float height, const glm::vec4 &color) {
+   Vertex v1, v2, v3, v4;
+
+   v1.position = { x, y, 0.0f };
+   v1.color    = color;
+
+   v2.position = { x + width, y, 0.0f };
+   v2.color    = color;
+
+   v3.position = { x + width, y + height, 0.0f };
+   v3.color    = color;
+
+   v4.position = { x, y + height, 0.0f };
+   v4.color    = color;
+
+   return { v1, v2, v3, v4 };
+}
+
+/* ------------------------------------------------------------------------------------------------------- */
