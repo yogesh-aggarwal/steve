495c59403a91ff6bb8ed83c5df29099dff3f701d
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-12-12 14:52:02 +0530

    Add necessary graphics headers for rendering and context management

--- source/steve/steve.hpp
+++ source/steve/steve.hpp
@@ -2,6 +2,17 @@
 
 #include <steve/core/pch>
 
+#include <steve/lib/application/window.hpp>
+
+#include <steve/lib/graphics/draw.hpp>
+#include <steve/lib/graphics/shaders.hpp>
+#include <steve/lib/graphics/renderer/renderer.hpp>
+#include <steve/lib/graphics/renderer/vertex.hpp>
+#include <steve/lib/graphics/renderer/backend/backend.hpp>
+#include <steve/lib/graphics/renderer/context/graphics.hpp>
+#include <steve/lib/graphics/renderer/context/opengl.hpp>
+#include <steve/lib/graphics/renderer/context/vulkan.hpp>
+
 /* ------------------------------------------------------------------------------------------------------- */
 
 inline void
